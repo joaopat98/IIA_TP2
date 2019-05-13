71c679cb44e57b31b8e539eedd5f553059ba9414
João Patrício <joaopat98@gmail.com>
2019-05-13 18:36:33 +0100

    started genetic algorithm, need to decide how to select from offspring

--- Assets/Scripts/LearningAlgorithms/GeneticAlgorithm.cs
+++ Assets/Scripts/LearningAlgorithms/GeneticAlgorithm.cs
@@ -2,21 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneticAlgorithm : MetaHeuristic {
-	public float mutationProbability;
-	public float crossoverProbability;
-	public int tournamentSize;
-	public bool elitist;
+public class GeneticAlgorithm : MetaHeuristic
+{
 
-	public override void InitPopulation () {
-		//You should implement the code to initialize the population here
-		throw new System.NotImplementedException ();
-	}
+    private class IndividualComparer : IComparer<Individual>
+    {
+        public int Compare(Individual x, Individual y)
+        {
+            return x.Fitness > y.Fitness ? -1 : 1;
+        }
+    }
 
-	//The Step function assumes that the fitness values of all the individuals in the population have been calculated.
-	public override void Step() {
-		//You should implement the code runs in each generation here
-		throw new System.NotImplementedException ();
-	}
+    public float mutationProbability;
+    public float crossoverProbability;
+    public int tournamentSize;
+    public bool elitist;
+
+    public override void InitPopulation()
+    {
+        population = new List<Individual>();
+        // jncor 
+        while (population.Count < populationSize)
+        {
+            GeneticIndividual new_ind = new GeneticIndividual(topology);
+            new_ind.Initialize();
+            population.Add(new_ind);
+        }
+
+    }
+
+    //The Step function assumes that the fitness values of all the individuals in the population have been calculated.
+    public override void Step()
+    {
+        population.Sort(new IndividualComparer());
+        List<Individual> offspring = new List<Individual>();
+        for (int i = 0; i < tournamentSize; i++)
+        {
+            Individual child = population[i].Clone();
+            child.Crossover(population[(i + 1) % tournamentSize], 0);
+            offspring.Add(child);
+            child = population[i].Clone();
+            child.Crossover(population[(i + 2) % tournamentSize], 0);
+            offspring.Add(child);
+        }
+		
+        //implement selection from offspring / parents
+        throw new System.NotImplementedException();
+    }
 
 }
--- Assets/Scripts/LearningAlgorithms/GeneticIndividual.cs
+++ Assets/Scripts/LearningAlgorithms/GeneticIndividual.cs
@@ -2,36 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneticIndividual : Individual {
+public class GeneticIndividual : Individual
+{
 
 
-	GeneticIndividual(int[] topology) : base(topology) {
-	}
+    public GeneticIndividual(int[] topology) : base(topology)
+    {
+    }
 
-	public override void Initialize () 
-	{
-		throw new System.NotImplementedException ();
-	}
-		
-	public override void Crossover (Individual partner, float probability)
-	{
-		throw new System.NotImplementedException ();
-	}
+    public override void Initialize()
+    {
+        for (int i = 0; i < totalSize; i++)
+        {
+            genotype[i] = Random.Range(-1.0f, 1.0f);
+        }
+    }
 
-	public override void Mutate (float probability)
-	{
-		throw new System.NotImplementedException ();
-	}
+    public override void Crossover(Individual partner, float probability)
+    {
+        int first, second;
+        first = Random.Range(0, genotype.Length - 2);
+        second = Random.Range(first + 1, genotype.Length - 1);
+		for (int i = first; i <= second; i++)
+		{
+			genotype[i] = ((GeneticIndividual) partner).genotype[i];
+		}
+    }
 
-	public override Individual Clone ()
-	{
-		GeneticIndividual new_ind = new GeneticIndividual(this.topology);
+    public override void Mutate(float probability)
+    {
+        for (int i = 0; i < totalSize; i++)
+        {
+            if (Random.Range(0.0f, 1.0f) < probability)
+            {
+                genotype[i] = Random.Range(-1.0f, 1.0f);
+            }
+        }
+    }
 
-		genotype.CopyTo (new_ind.genotype, 0);
-		new_ind.fitness = this.Fitness;
-		new_ind.evaluated = false;
+    public override Individual Clone()
+    {
+        GeneticIndividual new_ind = new GeneticIndividual(this.topology);
 
-		return new_ind;
-	}
+        genotype.CopyTo(new_ind.genotype, 0);
+        new_ind.fitness = this.Fitness;
+        new_ind.evaluated = false;
+
+        return new_ind;
+    }
 
 }
